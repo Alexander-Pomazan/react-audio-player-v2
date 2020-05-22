0d1db135a2df313d42e95e1725a577aa80db67ab
Alexander Pomazan <alexander.pomazan@quantori.com>
2020-05-23 00:13:23 +0300

    feat: add global styles

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,16 @@
 import React, { StrictMode } from 'react'
 import { render } from 'react-dom'
-import 'reset-css'
+import { Reset } from 'styled-reset'
+
+import { GlobalStyle } from './global-style'
 
 import { App } from 'src/app'
 
 render(
   <StrictMode>
+    <Reset />
+    <GlobalStyle />
+
     <App />
   </StrictMode>,
   document.getElementById('root'),
--- src/player.tsx
+++ src/player.tsx
@@ -1,16 +1,26 @@
 import React from 'react'
+import styled from 'styled-components'
 
 import { useLoadTracks } from 'src/hooks'
 import { List } from 'src/ui'
+
+const Root = styled.div`
+  width: 800px;
+  max-width: 90%;
+  margin-left: auto;
+  margin-right: auto;
+`
 
 export const Player: React.FC = () => {
   const tracks = useLoadTracks('/tracks.json')
 
   return (
-    <List direction='column' gap={20}>
-      {tracks.map((track) => (
-        <pre key={track.id}>{JSON.stringify(track, null, 4)}</pre>
-      ))}
-    </List>
+    <Root>
+      <List direction='column' gap={40}>
+        {tracks.map((track) => (
+          <span key={track.id}>{JSON.stringify(track, null, 8)}</span>
+        ))}
+      </List>
+    </Root>
   )
 }
